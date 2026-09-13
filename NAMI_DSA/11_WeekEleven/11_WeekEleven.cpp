e7f990bae7885bc4135aebf05d5bee935f6171c3
Chandra Mohan Jayaswal <chandra.ets@gmail.com>
2026-09-13 08:34:06 +0545

    update

--- NAMI_DSA/11_WeekEleven/11_WeekEleven.cpp
+++ NAMI_DSA/11_WeekEleven/11_WeekEleven.cpp
@@ -36,7 +36,7 @@ struct StackNode {
         previousNode->nextAddress = nullptr;
         return currentNode;
     }
-    void displayNode(){
+    void display(){
         cout <<"List: ";
         StackNode *currentNode = this;
         while (currentNode != nullptr) {
@@ -47,47 +47,15 @@ struct StackNode {
     }
 };
 
-//void push(StackNode *head, StackNode *newNode) {
-//    //Need to move at the end
-//    StackNode *currentNode = head;
-//    while (currentNode->nextAddress != nullptr) {
-//        currentNode = currentNode->nextAddress;
-//    }
-//    //Add new node at the end
-//    currentNode->nextAddress = newNode;
-//}
-
-//StackNode* pop(StackNode *head) {
-//    //Need to move at the end
-//    StackNode *previousNode = nullptr;
-//    StackNode *currentNode = head;
-//    while (currentNode->nextAddress != nullptr) {
-//        previousNode = currentNode;
-//        currentNode = currentNode->nextAddress;
-//    }
-//    previousNode->nextAddress = nullptr;
-//    return currentNode;
-//}
-
-//void displayNode(StackNode *headNode){
-//    cout <<"List: ";
-//    StackNode *currentNode = headNode;
-//    while (currentNode != nullptr) {
-//        cout << currentNode->data << " ";
-//        currentNode = currentNode->nextAddress;
-//    }
-//    cout << endl;
-//}
-
 int main() {
     StackNode *node1 = new StackNode(10, nullptr);
     node1->push(new StackNode(20, nullptr));
     node1->push(new StackNode(30, nullptr));
     node1->push(new StackNode(40, nullptr));
-    node1->displayNode();
+    node1->display();
     StackNode *node = node1->pop();
     cout <<"Poped Data: " << node->data << endl;
-    node1->displayNode();
+    node1->display();
 
     return 0;
 }
